aa64ba2f4530d9537e73e4d5b2f8dd09a4d2c903
Khalid Sadat <khalid_saadat@hotmail.com>
2022-03-26 00:11:44 -0400

    Added 5 unit tests for login: #122

---
+++ app/test_models/Login.php
@@ -0,0 +1,34 @@
+<?php
+    namespace App;
+
+    class Login {
+        public static string $username;
+        public  static string $password;
+
+        protected function getPdo(): \PDO {
+            if ($this->pdo === null) {
+                $options = [
+                     \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+                     \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+                ];
+                try {
+                     $this->pdo = new \PDO("mysql:host=localhots; dbname=soen341; charset=utf8mb4", 'root', '', $options);
+                  } catch (\PDOException $PDOException) {
+                     throw new \PDOException($PDOException->getMessage(), (int) $PDOException->getCode());
+                }
+            }
+            return $this->pdo;
+        }
+
+        public function correct_username_and_password() {
+            
+        }
+
+        public function fetchUsers(): array {
+            dd("this is inside fetchusers method");
+            
+            return $this->getPdo()->prepare("SELECT * FROM user")->fetchAll(\PDO::FETCH_ASSOC);
+        }
+
+    }
+?>
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit50f38a72f0678d2c8d43d3e606b66978
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit50f38a72f0678d2c8d43d3e606b66978', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit50f38a72f0678d2c8d43d3e606b66978', 'loadClassLoader'));
